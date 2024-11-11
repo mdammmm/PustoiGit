edf2a41cae10befb0f935deabdf1e3e68dc90a2d
mdammmm <101309928+mdammmm@users.noreply.github.com>
2024-11-11 21:54:06 +0900

    Добавлены комментарии в код

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,7 @@
 def main():
+    # Запрос имени пользователя
     name = input("Введите ваше имя: ")
+    # Вывод приветствия
     print("Hello world from " + name + "!")
 
 if __name__ == "__main__":
